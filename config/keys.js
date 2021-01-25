6bdc40383b66ef04bd851de6cb29089e9dcfa65a
SIDDHARTHA VIBHU PHARSWAN <spharswan@ptc.com>
2021-01-25 15:09:41 +0530

    new commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'prod') {
     module.exports = require('./keys_prod');
 } else {
     module.exports = require('./keys_dev');
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const mongoose = require('mongoose');
@@ -19,7 +20,6 @@ app.use(bodyParser.json({
 require('./config/passportConfig')(passport);
 
 const port = process.env.PORT || 5000;
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const mongodbURI = require('./config/keys').mongodbURI;
 
@@ -32,12 +32,17 @@ mongoose.connect(mongodbURI, {
     console.error(err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
 
 app.use('/api/employees', employees);
 app.use('/api/orders', orders);
+
+if (process.env.NODE_ENV === 'prod') {
+    app.use(express.static('office-cafeteria/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'office-cafeteria', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Cafeteria Server is running on ${port}`);
